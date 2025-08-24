cbcc8b398067ff6ed323ce33505430396643ce19
leehyunmin <leehm2292@gmail.com>
2025-08-24 16:59:49 +0900

    images

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:webtoonflix/models/webtoon_model.dart';
+import 'package:webtoonflix/services/api_service.dart';
+
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
+
+  final Future<List<WebtoonModel>> webtoons = Apiservice.getTodaysToons();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        shadowColor: Colors.black,
+        elevation: 0.5,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.green,
+        title: Center(
+          child: Text(
+            "오늘의 웹툰",
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.w400),
+          ),
+        ),
+      ),
+      body: FutureBuilder(
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Column(
+              children: [
+                SizedBox(height: 40,),
+                Expanded(child: makeList(snapshot))
+              ],
+            );
+          }
+          return Center(child: CircularProgressIndicator());
+        },
+        future: webtoons,
+      ),
+    );
+  }
+
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data!.length,
+      itemBuilder: (context, index) {
+        var webtoon = snapshot.data![index];
+        return Column(
+          children: [
+            SizedBox(
+              width: 250,
+              child: Image.network(webtoon.thumb),
+            ),
+            Text(webtoon.title),
+          ],
+        );
+      },
+      separatorBuilder: (context, index) => SizedBox(width: 40),
+    );
+  }
+}
